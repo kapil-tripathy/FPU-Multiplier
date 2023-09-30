423718094e6a2d29e36abac7e212219a004c4ddb
kapil-tripathy <59687305+kapil-tripathy@users.noreply.github.com>
2023-09-30 19:44:46 +0530

    2nd commit, commiting the tb for hp_class module

---
+++ FPU.srcs/sim_1/new/hp_class_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.09.2023 19:10:09
+// Design Name: 
+// Module Name: hp_class_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module hp_class_tb();
+
+
+endmodule
--- FPU.srcs/sources_1/new/hp_class.v
+++ FPU.srcs/sources_1/new/hp_class.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Kapil Tripathy 
 // 
 // Create Date: 29.09.2023 20:09:40
-// Design Name: 
+// Design Name: Floating POint clasifier
 // Module Name: hp_class
-// Project Name: 
+// Project Name: FPU Multiplier
 // Target Devices: 
 // Tool Versions: 
 // Description: 
@@ -20,7 +20,29 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module hp_class(
+module hp_class(f, sNaN, qNaN, infinity, zero, normal, subNormal);
 
-    );
+input [15:0] f;
+output sNaN;
+output qNaN;
+output infinity;
+output zero;
+output normal;
+output subNormal;
+
+wire expOnes;
+wire expZeros;
+wire sigZeros;
+
+assign expOnes = &f[14:10];
+assign expZeros = ~|f[15:10];
+assign sigZeros = ~|f[9:0];
+
+assign sNaN = expOnes & ~sigZeros & ~f[9];
+assign qNaN = expOnes & f[9];
+assign infinity = expOnes & sigZeros;
+assign zero = expZeros & sigZeros;
+assign normal =  ~expOnes & ~expZeros;
+assign subNormal = expZeros & ~sigZeros; 
+
 endmodule
